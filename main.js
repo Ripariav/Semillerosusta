ec8f460684821d3c288c7d5cdf8118d47ef13273
Ripariav <68032529+Ripariav@users.noreply.github.com>
2023-06-01 03:36:09 +0000

    Deploying to gh-pages from @ Ripariav/Semillerosusta@8cdfc51fe76727a7b3a7a575ce4b88dd4244a511 🚀

--- main.js
+++ main.js
@@ -35,6 +35,45 @@ ScrollReveal().reveal('.scr',{
     
 })
 
+ScrollReveal().reveal('.scb-dl-1',{
+    delay: 300,
+    distance: '50px',
+    duration: 2500,
+    reset: false
+    
+})
+
+ScrollReveal().reveal('.scb-dl-2',{
+    delay: 750,
+    distance: '50px',
+    duration: 2500,
+    reset: false
+    
+})
+
+ScrollReveal().reveal('.scb-dl-3',{
+    delay: 1000,
+    distance: '50px',
+    duration: 2500,
+    reset: false
+    
+})
+
+ScrollReveal().reveal('.bloquesIndustrial',{
+    delay:500, 
+    reset:false,
+    origin: 'bottom',
+    distance: '20px'
+});
+
+ScrollReveal().reveal('.infoSemestreCarrera',{
+    delay:800, 
+    reset:false,
+    origin: 'right',
+    distance: '50px'
+});
+
+
 //carrusel con swiper js
 const swiper = new Swiper('.galeriaUsta', {
     // Optional parameters
